a1e480d67a329b4d045cc49b6f3ec4a84127d294
Chris Stroud <chris.stroud00@gmail.com>
2021-03-08 21:41:09 +0000

    add profile component

---
+++ components/HeroProfile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ hero.name }}</h2>
+
+    <div>Dexterity: {{ hero.dexterity }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroProfile',
+  props: {
+    hero: {
+      type: Object,
+      required: true,
+    }
+  }
+}
+</script>
